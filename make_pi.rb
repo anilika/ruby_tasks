9e0bd1c7f7fc9701b9ad970753e7e2216ccdda8e
Anilika <alisa88_17@inbox.ru>
2016-01-13 00:28:03 +1100

    Added description

--- make_pi.rb
+++ make_pi.rb
@@ -1,10 +1,11 @@
 #! /bin/env ruby
+# Print first three digits of Pi
 
 def make_pi
-	new_pi = Math::PI.round(2)
-	array = (new_pi.to_s).split("")
-	array.delete(".")
-	print array
+  new_pi = Math::PI.round(2)
+  array = (new_pi.to_s).split("")
+  array.delete(".")
+  print array
 end
 
 make_pi
